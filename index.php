50ba550d97688e5bcc83c8d4ae9619eaada18283
METADETRON <contact@metadetron.com>
2016-09-23 00:27:47 +0200

    dummy rest

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+	header("Access-Control-Allow-Orgin: *");
+    header("Access-Control-Allow-Methods: *");
+    header("Content-Type: application/json");
+	$method = $_SERVER['REQUEST_METHOD'];
+	if ($method == "GET") {
+		if (is_null($_GET['entity'])) {
+			header("HTTP/1.1 406 Missing entity information");
+			return;
+		}
+		$entity = $_GET['entity'];
+		if ($entity == "auth") {
+            echo json_encode("lol");
+            exit;
+        }
+    }
+?>
