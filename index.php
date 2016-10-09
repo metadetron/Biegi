e089eb1908b810a191b1d837d416e524d3ffcf4e
METADETRON <contact@metadetron.com>
2016-10-09 21:18:57 +0200

    Europe/Warsaw

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
  */ 
 
 	session_start();
+	date_default_timezone_set('Europe/Warsaw');
 	header("Access-Control-Allow-Orgin: *");
     header("Access-Control-Allow-Methods: *");
     header("Content-Type: application/json");
